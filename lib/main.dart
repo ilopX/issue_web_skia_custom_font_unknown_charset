606a286e0ac36df98940d3607aaf1953a4628a40
ilopX <ilopXgroup@gmail.com>
2020-08-15 16:33:39 +0300

    web skia unknown characters "(", ")"

--- lib/main.dart
+++ lib/main.dart
@@ -7,44 +7,93 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final defaultFont = TextStyle(
+      fontFamily: 'Sergeo UI',
+      fontSize: 30,
+    );
+    final customFont = TextStyle(
+      fontFamily: 'Segoe_form_assets',
+      fontSize: 30,
+    );
+
     return MaterialApp(
       theme: ThemeData.dark(),
       home: Scaffold(
-        body: Center(
-          child: Container(
-            height: 70,
-            color: Colors.grey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  color: Colors.blue,
-                  child: Text(
-                    '01: Default font [ end: Alignment(-1, -1) ]',
-                  ),
-                ),
-                Container(
-                  color: Colors.amber,
-                  child: Text(
-                    '02: Custom font [  end: Alignment(-1, -1) ]',
-                    style: const TextStyle(fontFamily: 'Arial'),
-                  ),
-                ),
-                Container(
-                  color: Colors.green,
-                  child: Text(
-                    '03: Import asset custom font [ end: Alignment(-1, -1) ]',
-                    style: const TextStyle(
-                      fontFamily: 'Segoe',
-                      color: Colors.white,
+        body: Column(
+          children: [
+            buildHeader(['default', 'custom']),
+            buildCharCodes(
+                startChar: ' ',
+                endChar: '~',
+                charBuilder: (char) {
+                  return [
+                    Text(char, style: defaultFont),
+                    buildErrorIfUnknownChars(
+                        char: char, child: Text(char, style: customFont)
                     ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+                  ];
+                }),
+          ],
         ),
       ),
     );
+  }
+
+  Widget buildHeader(List<String> headerNames) {
+    return Container(
+      color: Colors.brown,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          for (final header in headerNames)
+            Text(
+              header,
+              style: TextStyle(fontFamily: 'Arial', fontSize: 20),
+            ),
+        ],
+      ),
+    );
+  }
+
+  buildCharCodes({
+    String startChar,
+    String endChar,
+    List<Widget> Function(String char) charBuilder,
+  }) {
+    final startCode = startChar.codeUnitAt(0);
+    final endCode = endChar.codeUnitAt(0);
+    assert(startCode < endCode);
+
+    return Expanded(
+      child: ListView(
+        children: [
+          for (int code = startCode; code < endCode; code++)
+            buildCode(code, charBuilder)
+        ],
+      ),
+    );
+  }
+
+  Widget buildCode(int code, List<Widget> Function(String char) charBuilder) {
+    final char = String.fromCharCode(code);
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: charBuilder(char),
+        ),
+        Divider(),
+      ],
+    );
+  }
+
+  Widget buildErrorIfUnknownChars({String char, Widget child}) {
+    return '()'.contains(char)
+        ? Container(
+
+            color: Colors.red.withOpacity(0.7),
+            child: child,
+          )
+        : child;
   }
 }
